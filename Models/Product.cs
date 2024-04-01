304513f53b120e14a456224d907e8c5c059010e2
Rithy <huotrithy01@gmail.com>
2024-04-01 21:23:59 +0700

    fix update product

--- Models/Product.cs
+++ Models/Product.cs
@@ -30,7 +30,8 @@ namespace EcommerceWebAppProject.Models
 		public double Price { get; set; }
 
 		public int CatId { get; set; }
-		[ForeignKey("CatId")]		
+		[ForeignKey("CatId")]
+		[ValidateNever]
         public Category Category { get; set; }
 		
 		public string? ImageUrl { get; set; }
