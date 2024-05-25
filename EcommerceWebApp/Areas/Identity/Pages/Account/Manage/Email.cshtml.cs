2aa8993024d8e8c79fa67ebe1516c68760b80587
Rithy <huotrithy01@gmail.com>
2024-05-25 17:56:19 +0700

    remove unnecessary feature

--- EcommerceWebApp/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
+++ EcommerceWebApp/Areas/Identity/Pages/Account/Manage/Email.cshtml.cs
@@ -115,21 +115,25 @@ namespace EcommerceWebApp.Areas.Identity.Pages.Account.Manage
 
             var email = await _userManager.GetEmailAsync(user);
             if (Input.NewEmail != email)
-            {
-                var userId = await _userManager.GetUserIdAsync(user);
-                var code = await _userManager.GenerateChangeEmailTokenAsync(user, Input.NewEmail);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmailChange",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = userId, email = Input.NewEmail, code = code },
-                    protocol: Request.Scheme);
-                await _emailSender.SendEmailAsync(
-                    Input.NewEmail,
-                    "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+            {                
 
-                StatusMessage = "Confirmation link to change email sent. Please check your email.";
+                // set the email on the user, but don't send any email
+                await _userManager.SetEmailAsync(user, Input.NewEmail);
+
+                //generate an email confirmation token
+                var emailConfirmationToken = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
+                //confirm the new email right away
+                var result = await _userManager.ConfirmEmailAsync(user, emailConfirmationToken);
+                                
+                if (!result.Succeeded)
+                {
+                    StatusMessage = "Error changing user email.";
+                    return RedirectToPage();
+                }
+
+                await _signInManager.RefreshSignInAsync(user);
+                StatusMessage = "Changing user email was successful.";
                 return RedirectToPage();
             }
 
